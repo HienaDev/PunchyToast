10936e3bc7967d476cce97877e474ba24312a007
HienaDev <ant.rodrigues1999@gmail.com>
2026-05-03 11:15:31 +0100

    Add reset to combo

--- Assets/ResetProgress.cs
+++ Assets/ResetProgress.cs
@@ -5,22 +5,31 @@ public class ResetProgress : MonoBehaviour
 {
     public void ResetLevelProgress()
     {
-
+        // Load all level configurations from the Resources/Levels folder
         LevelConfiguration[] allConfigs = Resources.LoadAll<LevelConfiguration>("Levels");
-
 
         foreach (var config in allConfigs)
         {
+            int num = config.levelNumber;
+
             // Delete the Stars entry
-            PlayerPrefs.DeleteKey($"Level_{config.levelNumber}_Stars");
+            PlayerPrefs.DeleteKey($"Level_{num}_Stars");
 
             // Delete the Time entry
-            PlayerPrefs.DeleteKey($"Level_{config.levelNumber}_Time");
+            PlayerPrefs.DeleteKey($"Level_{num}_Time");
+
+            // Delete the Highest Combo entry
+            PlayerPrefs.DeleteKey($"Level_{num}_Combo");
         }
 
-        // Optional: Reset the "Last Played" tracking
+        // Reset the "Last Played" tracking
         PlayerPrefs.DeleteKey("LastPlayedLevel");
 
+        // Optional: If you have a global highest combo (not per level), delete it here:
+        // PlayerPrefs.DeleteKey("GlobalHighestCombo");
+
         PlayerPrefs.Save();
+
+        Debug.Log("All level progress, times, and combo records have been wiped.");
     }
 }
